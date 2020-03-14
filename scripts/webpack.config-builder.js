821f204456f9fda4959b468e3781549ea2be9ed2
Amr Noman <amrnn90@gmail.com>
2020-03-15 00:14:01 +0300

    save chunks in js/ dir

--- scripts/webpack.config-builder.js
+++ scripts/webpack.config-builder.js
@@ -131,7 +131,7 @@ module.exports = ({
         path: outputAbsPath,
         publicPath: "/",
         filename: `js/${jsBundleName}`,
-        chunkFilename: "[name].js",
+        chunkFilename: "js/[name].js",
       },
       module: {
         rules: [
